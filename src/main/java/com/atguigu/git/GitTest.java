920dbe6ff01a4fbcb9e8b4bb7a04f09f144609fc
HX <3254760462@qq.com>
2022-07-31 16:13:46 +0800

    分支

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("第二次更新");
         System.out.println("第三次更新");
         System.out.println("第四次更新");
+        System.out.println("分支");
     }
 }
---
+++ src/main/java/com/atguigu/git/NewFile.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author： HX
+ * @date: 2022/7/31
+ * description
+ */
+public class NewFile {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
